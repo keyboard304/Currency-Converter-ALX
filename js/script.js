80caa21ca27fe87baa59fab831cd5c05f279c370
keyboard304 <youssifyahia2004@gmail.com>
2025-09-07 22:10:57 +0300

    Populate the currency dropdown, attach selected element change event handler

--- js/script.js
+++ js/script.js
@@ -7,6 +7,20 @@ const exchangeIcon = document.querySelector("form .icon");
 const amount = document.querySelector("form input");
 const exchangeRateTxt = document.querySelector("form .exchange-rate");
 
+// Populate currency dropdowns
+for (let i = 0; i < dropList.length; i++) {
+    for (let currency_code in country_list) {
+        // Set USD as default FROM currency and EGP as default TO currency
+        let selected = i == 0 ? currency_code == "USD" ? "selected" : "" : currency_code == "EGP" ? "selected" : "";
+        let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+        dropList[i].insertAdjacentHTML("beforeend", optionTag);
+    }
+    // Add change event listener to update flags
+    dropList[i].addEventListener("change", e => {
+        loadFlag(e.target);
+    });
+}
+
 // Function to load appropriate flag based on currency selection
 function loadFlag(element) {
     for (let code in country_list) {
